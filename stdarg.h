04abac6b4925a5e76334ba6701c94ce1e64c03ff
kobenairb <kobenairb@gmail.com>
2022-09-24 08:47:57 +0800

    Tinycc for WDC 65816 by Alekmaul [0.9.23]

---
+++ stdarg.h
@@ -0,0 +1,22 @@
+#ifndef _STDARG_H
+#define _STDARG_H
+
+typedef char *va_list;
+
+#ifdef __65816__
+#define va_start(ap, last) ap = ((char *)&(last)) + sizeof(last)
+#define va_arg(ap, type) (ap += sizeof(type), *(type *)(ap - sizeof(type)))
+#define va_end(ap)
+#define va_copy(aq, ap) ((aq) = (ap))
+#else
+/* only correct for i386 */
+#define va_start(ap, last) ap = ((char *)&(last)) + ((sizeof(last) + 3) & ~3)
+#define va_arg(ap, type) (ap += (sizeof(type) + 3) & ~3, *(type *)(ap - ((sizeof(type) + 3) & ~3)))
+#define va_end(ap)
+
+/* fix a buggy dependency on GCC in libio.h */
+typedef va_list __gnuc_va_list;
+#endif
+#define _VA_LIST_DEFINED
+
+#endif
