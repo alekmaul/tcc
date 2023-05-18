0577f3d86a1fd7cf3c45aec9b5220d20676b3064
kobenairb <kobenairb@gmail.com>
2023-05-18 09:36:19 +0800

    clang-format on files Sat Sep 17 02:51:54 PM PST 2022

--- stdarg.h
+++ stdarg.h
@@ -2,7 +2,6 @@
 #define _STDARG_H
 
 typedef char *va_list;
-typedef va_list __builtin_va_list;
 
 #ifdef __65816__
 #define va_start(ap, last) ap = ((char *) &(last)) + sizeof(last)
@@ -18,11 +17,6 @@ typedef va_list __builtin_va_list;
 /* fix a buggy dependency on GCC in libio.h */
 typedef va_list __gnuc_va_list;
 #endif
-
-#define __builtin_va_start va_start
-#define __builtin_va_arg va_arg
-#define __builtin_va_end va_end
-
 #define _VA_LIST_DEFINED
 
 #endif
